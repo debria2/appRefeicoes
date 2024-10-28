2eeca408292f31f8373cfaa8d9345e4ec8dc7cc5
debria2 <ninadbm6@gmail.com>
2024-10-28 20:40:11 -0300

    text do commit

--- firebase.js
+++ firebase.js
@@ -1,21 +1,27 @@
 import { initializeApp } from "firebase/app"; 
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // Importar getAuth
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, initializeAuth, getReactNativePersistence } from "firebase/auth"; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkD9t2UVpZggRwGc84q4a85ropXPSUXe8",
- 
   authDomain: "apprefeicaoaline.firebaseapp.com",
- 
   projectId: "apprefeicaoaline",
- 
   storageBucket: "apprefeicaoaline.appspot.com",
- 
   messagingSenderId: "195867651474",
- 
   appId: "1:195867651474:web:070e3e74749861111f654b"
-  };
+};
 
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializar Firestore
 export const firestore = getFirestore(app);
-export const auth = getAuth(app); // Exportar auth
+
+// Inicializar Auth com persistência
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Exportar funções de autenticação
+export { signInWithEmailAndPassword, onAuthStateChanged };
